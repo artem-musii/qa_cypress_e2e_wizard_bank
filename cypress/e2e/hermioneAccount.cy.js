9560515610d0e35956ab830b8ee20e7496e9396a
Artem Musii <musiy.artem@gmail.com>
2024-07-07 21:46:14 +0200

    added hermione account test

---
+++ cypress/e2e/hermioneAccount.cy.js
@@ -0,0 +1,53 @@
+/// <reference types='cypress' />
+
+const { faker } = require('@faker-js/faker');
+
+describe('Bank app', () => {
+  const depositAmount = `${faker.number.int({ min: 500, max: 1000 })}`;
+  const withdrawAmount = `${faker.number.int({ min: 50, max: 500 })}`;
+  const baseBalance = 5096;
+  const balance = depositAmount - withdrawAmount + baseBalance;
+  const user = 'Hermoine Granger';
+  const accountNumber = '1001';
+
+  before(() => {
+    cy.visit('/');
+  });
+
+  it('should provide the ability to work with Hermione\'s bank account', () => {
+    cy.contains('.btn', 'Customer Login').click();
+    cy.get('[name="userSelect"]').select(user);
+    cy.contains('.btn', 'Login').click();
+
+    cy.contains('[ng-hide="noAccount"]', 'Account Number')
+      .contains('strong', accountNumber)
+      .should('be.visible');
+    cy.contains('[ng-hide="noAccount"]', 'Balance')
+      .contains('strong', baseBalance)
+      .should('be.visible');
+    cy.contains('.ng-binding', 'Dollar')
+      .should('be.visible');
+
+    cy.get('[ng-click="deposit()"]').click();
+    cy.get('[placeholder="amount"]').type(depositAmount);
+    cy.contains('[type="submit"]', 'Deposit').click();
+
+    cy.get('[ng-show="message"]')
+      .should('contain', 'Deposit Successful');
+    cy.contains('[ng-hide="noAccount"]', 'Balance')
+      .contains('strong', +depositAmount + +baseBalance)
+      .should('be.visible');
+
+    cy.get('[ng-click="withdrawl()"]').click();
+    cy.contains('[type="submit"]', 'Withdraw')
+      .should('be.visible');
+    cy.get('[placeholder="amount"]').type(withdrawAmount);
+    cy.contains('[type="submit"]', 'Withdraw').click();
+
+    cy.get('[ng-show="message"]')
+      .should('contain', 'Transaction successful');
+    cy.contains('[ng-hide="noAccount"]', 'Balance')
+      .contains('strong', balance)
+      .should('be.visible');
+  });
+});
